22899a98dcdf50f7f03f4c3f585e8535a0d52998
Shamil Malinda <78814285+shamilmucha@users.noreply.github.com>
2025-05-13 23:38:33 +0530

    Update main.py
    updated with image downloading and printing information on the add into a text file. the script will create a folder to each ad and save the relevant text file and images on that directory

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import time
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
@@ -5,8 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
-import time
-
+import wget
 
 # Set up Firefox options
 
@@ -61,11 +62,17 @@ print(f"found {len(ads)}.ads")
 
 url_list = []
 
+
+
 for ad in ads:
     ad_link = ad.find_element(By.CSS_SELECTOR, 'a').get_attribute('href')
     #print(ad_link)
     url_list.append(ad_link)
-    print(url_list)
+
+print(url_list)
+
+
+
 
 #combing through the list and getting the table informaiton like price, contact info., condition, year, etc...
 
@@ -73,8 +80,18 @@ for ad_link in url_list:
     driver.get(ad_link)
     time.sleep(3)
 
+    #setting a location for images and info.
+
+    os.makedirs('downloaded images' + ad_link, exist_ok= True)
+    path = os.path.join(os.getcwd(),'downloaded images' + ad_link)
+    print(path)
+
+
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     table = soup.find('table', class_='moret')
+
+    #printing the info and exporting it to a text file
+
 
     if table:
         for row in table.find_all('tr'):
@@ -82,10 +99,59 @@ for ad_link in url_list:
             for cell in row.find_all(['tr', 'td']):
                 row_data.append(cell.get_text(strip=True))
             print(row_data)
+
+            file = open(path + '/info.txt', 'a')
+            file.write(str(row_data) + '\n')
+            file.close()
             
     else:
         print("Table not found")
+        file = open(path + '/info.txt', 'a')
+        file.write("Table not found" + '\n')
+        file.close()
         
     time.sleep(2)
 
+
+    #getting the first ad and opening it
+
+    image_slider = driver.find_element(By.XPATH, '//*[@id="thumbs"]')
+
+    #locating the thumbnail element
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    thumbs = soup.find_all('div', class_ = 'thumb')
+
+    #printing thumbs count
+
+    print(f"Found {len(thumbs)} thumbs")
+    time.sleep(2)
+
+    #creating image url list
+
+    image_url_list = []
+
+    #getting the url of the main images from the 'alt' option from the thumb
+
+    if len(thumbs) > 0:
+        for i, thumb in enumerate(thumbs, start= 1):
+            img_url = soup.find_all('img')[i]['alt']
+            image_url_list.append(img_url)
+    else:
+        print("No images found in the thumbnail section.")
+
+    #printing the image url list
+
+    image_url_list.pop(0)
+    print(image_url_list)
+    time.sleep(2)
+
+    #downloading the images
+
+    for img_url in image_url_list:
+        wget.download(img_url, path)
+        time.sleep(2)
+
+
+# Close the browser
 driver.quit()
